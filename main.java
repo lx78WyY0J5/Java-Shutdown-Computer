4c2e55437a853339034b4d21903831fa5f15405e
lx78WyY0J5 <9j2k37st@gmail.com>
2022-12-09 07:17:29 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args) throws Exception {
+        boolean instant = false;
+        boolean restart = false;
+        int time = 5; // sec
+
+        Runtime runtime = Runtime.getRuntime();
+
+        String argsWin = "";
+        if (restart) {
+            argsWin += " -r";
+        } else {
+            argsWin += " -s";
+        }
+
+        if (!instant) {
+            argsWin += " -t " + time;
+        }
+
+        try {
+            if (instant) {
+                runtime.exec("shutdown" + argsWin);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
